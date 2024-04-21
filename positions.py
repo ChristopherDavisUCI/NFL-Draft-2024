bafd92e607559d2b67ec292175825f8880dd1227
Christopher Davis <christopherjdavis@gmail.com>
2024-04-21 11:12:25 -0700

    Update positions.py

--- positions.py
+++ positions.py
@@ -5,7 +5,7 @@ import numpy as np
 
 st.set_page_config(layout="wide", page_title="2024 NFL Draft Positions")
 
-st.title('2024 NFL Draft Positions')
+st.title('2024 NFL Draft')
 
 df = pd.read_csv("data/draft2024e.csv")
 df["date"] = pd.to_datetime(df["date"])
@@ -19,8 +19,9 @@ def get_rank(df_mini, player):
     except:
         return None
 
-def make_chart(pos, df):
-    dfpos = df[df["position"] == pos].copy()
+# option can be "position" or "conference"
+def make_chart(pos, df, option="position"):
+    dfpos = df[df[option] == pos].copy()
     players = dfpos.player.unique()
     df_list = []
     for _, df_mini in dfpos.groupby("url", sort=None):
@@ -35,9 +36,10 @@ def make_chart(pos, df):
                 new_row = sample.copy()
                 new_row["player"] = player
                 df_mini = pd.concat([df_mini, pd.DataFrame([new_row])])
+        df_mini["count"] = len(mini_players)
         df_list.append(df_mini)
     df_rank = pd.concat(df_list, axis=0)
-    return alt.Chart(df_rank).mark_line(point=True).encode(
+    base = alt.Chart(df_rank).encode(
         x=alt.X("draft-id:N", 
                 axis=alt.Axis(
                     labelAngle=-55, 
@@ -49,6 +51,11 @@ def make_chart(pos, df):
                     grid=True
                     ), 
                 sort=None),
+        tooltip = ["player", "pick", "position", "team", "source", "author", "date"],
+        href = "url:N"
+    )
+
+    points = base.mark_line(point=True).encode(
         y=alt.Y("pick:Q", scale=alt.Scale(reverse=True)),
         color=alt.Color(
             "player:N", 
@@ -56,25 +63,44 @@ def make_chart(pos, df):
             legend=alt.Legend(orient="left"),
             scale=alt.Scale(scheme="tableau20")
         ),
-        tooltip = ["player", "pick", "position", "source", "author", "date"],
-        href = "url:N"
-    ).properties(
+    )
+
+    text = base.mark_text().encode(
+        y = alt.value(-10),
+        text = "count"
+    )
+
+    chart = points+text
+    chart = chart.properties(
         title=pos,
         height=500,
     )
+    return chart
+
+default_authors = ["Andy Molitor", "Daniel Jeremiah", "Peter Schrager", "Charlie Campbell", "Dane Brugler", 
+                   "Matthew Freedman",    "Danny Kelly", "Benjamin Solak",
+               "Rob Staton",  "Jeff Risdon", "Walter Cherepinsky", "Trevor Sikkema",
+               "Shane Hallam"]
 
 # A few positions are missing
 positions = [pos for pos in df["position"].unique() if isinstance(pos, str)]
+conferences = sorted([conf for conf in df["conference"].unique() if isinstance(conf, str)])
 authors = sorted(df["author"].unique())
-
-pos = st.radio("What position?", positions, index = positions.index("QB"))
 
 chosen_authors = st.multiselect(
     "Choose your authors", 
     options = authors, 
-    default = ["Andy Molitor", "Dane Brugler", "Matthew Freedman", "Charlie Campbell", "Danny Kelly", "Benjamin Solak",
-               "Rob Staton", "Daniel Jeremiah", "Peter Schrager", "Jeff Risdon", "Walter Cherepinsky", "Trevor Sikkema",
-               "Shane Hallam"])
+    default = default_authors
+)
+
+st.header("Positions")
+
+pos = st.radio("What position?", positions, index = positions.index("QB"))
 
 st.altair_chart(make_chart(pos, df[df["author"].isin(chosen_authors)]))
 
+st.header("Conferences")
+
+conf = st.radio("What conference?", conferences)
+
+st.altair_chart(make_chart(conf, df[df["author"].isin(chosen_authors)], option="conference"))
